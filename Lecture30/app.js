8ccf8b38e5ed9f85aeab82755bd917f5aec5e88c
David Ruiz de la Rosa <ddlarosa@musicam.es>
2016-10-19 15:56:58 +0200

    Remove Item

--- Lecture30/app.js
+++ Lecture30/app.js
@@ -5,18 +5,39 @@
   .controller('ShoppingList1Controller',ShoppingList1Controller)
   .controller('ShoppingList2Controller',ShoppingList2Controller)
   .factory('ShoppingListFactory',ShoppingListFactory)
-  .directive('shoppingList',ShoppingList);
+  .directive('shoppingList',ShoppingListDirective);
 
-  function ShoppingList(){
+  function ShoppingListDirective(){
     var ddo={
       templateUrl:'shoppingList.html',
       scope:{
-        list:'=myList',
-        title:'@title'
-      }
+        items:'<',
+        title:'@',
+        onRemove:'&'
+      },
+      controller: ShoppingListDirectiveController,
+      controllerAs: 'list',
+      bindToController: true
     };
 
     return ddo;
+  };
+
+  function ShoppingListDirectiveController(){
+    var list=this;
+
+    list.cookiesInList=function(){
+      for(var i=0; i<list.items.length; i++){
+        var name=list.items[i].name;
+        if(name.toLowerCase().indexOf('cookie')!==-1){
+          return true;
+        };
+      };
+
+      return false;
+
+    };
+
   };
 
   ShoppingList1Controller.$inject=['ShoppingListFactory'];
